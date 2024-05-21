2b90cc3d4c2c6b9eff6f466b148589c8352b2011
Dzmitry Skryliou <dzmitry_skryliou@epam.com>
2024-05-21 14:51:46 +0200

    new repo

--- _netMentoringProgrammProject/Class1.cs
+++ _netMentoringProgrammProject/Class1.cs
@@ -2,6 +2,10 @@
 {
     public class Class1
     {
-
+        public static void Stars()
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
+                }
     }
 }
